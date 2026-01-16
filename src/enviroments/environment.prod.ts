15ff0afa814cb5e0087cce397fc06e5df06b1610
quatro00 <josecarlosgarciadiaz@gmail.com>
2026-01-15 22:42:31 -0600

    upd

--- src/enviroments/environment.prod.ts
+++ src/enviroments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://quatro0-001-site1.ktempurl.com/api'
+    apiUrl:'http://11.57.20.130:9099/api',
+    //apiUrl: 'https://quatro0-001-site1.ktempurl.com/api'
 };
--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7299/api'
+    apiUrl: 'http://11.57.20.130:9099/api',
+    //apiUrl: 'https://quatro0-001-site1.ktempurl.com/api'
 };
